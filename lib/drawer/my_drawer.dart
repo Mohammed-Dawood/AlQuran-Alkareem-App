afb78e06d5c0d87bb1b06eaec8d0f8ca8ebca12a
Mohammed Dawood <m.dawood.780909@gmail.com>
2023-03-30 21:47:23 +0200

    upgrade

--- lib/drawer/my_drawer.dart
+++ lib/drawer/my_drawer.dart
@@ -360,7 +360,7 @@ class _MyDrawerState extends State<MyDrawer> {
                 ),
               ),
               Text(
-                'الاصدار 1.2.0',
+                'الاصدار 1.3.0',
                 style: TextStyle(
                   fontSize: 20,
                   color: Color.fromRGBO(6, 87, 96, 1),
